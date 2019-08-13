9dd564a6f6964f1ba710369f4f425a461f551025
nguyenvanminhfpoly <minhnvph05683@fpt.edu.vn>
2019-08-13 16:33:20 +0700

    init

---
+++ AppBanDaQuy2-master/app/src/main/java/com/example/appbandaquy/Service/CheckConnection.java
@@ -0,0 +1,36 @@
+package com.example.appbandaquy.Service;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+public class CheckConnection {
+    public static boolean haveNetworkConnection(Context context) {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_CALENDAR)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+
+            NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+            for (NetworkInfo ni : netInfo) {
+                if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                    if (ni.isConnected())
+                        haveConnectedWifi = true;
+                if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                    if (ni.isConnected())
+                        haveConnectedMobile = true;
+            }
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
+    public static void showToast_short(Context context, String s){
+        Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
+    }
+}
